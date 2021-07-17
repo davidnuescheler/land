af131d6c37df31f152357d46dc24eea3d645ca56
Max Edell <maxed@adobe.com>
2021-07-16 17:18:31 -0700

    components stuff

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
-import { emit, initializeNamespaces } from './namespace';
+import { emit, initializeNamespaces } from './namespace.js';
 
 /**
  * Add dependency urls that should be published with Sidekick.
